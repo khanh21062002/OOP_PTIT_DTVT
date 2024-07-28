16f151b7935f381b003effe6af3288d442276ed7
haduykhanh <haduykhanh2106@gmail.com>
2024-07-28 16:03:23 +0700

    PTIT

---
+++ src/BASIC002/Main.java
@@ -0,0 +1,14 @@
+package BASIC002;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.nextLine());
+        while (t-- > 0) {
+            String s = sc.nextLine();
+            System.out.println(s);
+        }
+    }
+}
